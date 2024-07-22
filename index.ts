6548072210be1ac0be3328c88c9e0769831e5bfb
Fasih381 <socialmediaa381@gmail.com>
2024-07-22 07:20:21 -0700

    Create a Cli Guessing Game

---
+++ index.ts
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor( Math.random () * 10 +1 );
+
+const answers = await inquirer.prompt([
+   {
+    name: "GuessedNumber",
+    type: "number", 
+    message: "Please guess a Number between 1-10: ",
+   },
+]);
+
+if (answers.GuessedNumber === randomNumber) {
+    console.log("You Guessed Correct Number.");
+} else { 
+    console.log("You Guessed Wrong Number.");
+}
+
+console.log( "Correct Number is" , randomNumber );
